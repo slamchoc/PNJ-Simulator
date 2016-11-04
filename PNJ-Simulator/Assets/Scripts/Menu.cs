950667bf624e54d60ecde640ef37f49f6ce77ec6
Paul <paul.turpin@telecom-sudgrenoble.eu>
2016-11-05 00:31:08 +0100

    Menuing

---
+++ PNJ-Simulator/Assets/Scripts/Menu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+public class Menu : MonoBehaviour {
+
+    private Callback[] options;
+    private int position=0;
+
+    Menu(Delegate[] _options)
+    {
+        options = (Callback[])_options;
+    }
+
+	public void call()
+    {
+        options[position]();
+    }
+    public void incrementPosition()
+    {
+        position++;
+        if (position >= options.Length)
+            position = 0;
+    }
+    public void decrementPosition()
+    {
+        position--;
+        if (position <= 0)
+            position = options.Length - 1;
+    }
+}
