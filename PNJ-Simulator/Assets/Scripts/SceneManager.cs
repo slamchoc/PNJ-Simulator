43db9c7faa87cebce92053f2fd79e30913d947ae
pingu111 <theo.carron@telecom-sudparis.eu>
2016-11-04 23:20:22 +0100

    Scenes et scenemanager

---
+++ PNJ-Simulator/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneManager : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	
+    void goToMainMenu()
+    {
+
+    }
+
+    void goToBattle()
+    {
+
+    }
+
+    void goToMap()
+    {
+
+    }
+
+    void goToShop()
+    {
+
+    }
+
+
+}
