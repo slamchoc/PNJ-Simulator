8af433537bc37af8eaddd448526d25eeebd7aa42
pingu111 <theo.carron@telecom-sudparis.eu>
2016-11-04 23:29:37 +0100

    Maj events et scenes

--- PNJ-Simulator/Assets/Scripts/SceneManager.cs
+++ PNJ-Simulator/Assets/Scripts/SceneManager.cs
@@ -1,16 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public enum ScenesType
+{
+    SHOP,
+    MAP,
+    MAIN_MENU,
+    BATTLE
+}
+
 public class SceneManager : MonoBehaviour
 {
-
 	// Use this for initialization
 	void Start ()
     {
-	
+        DontDestroyOnLoad(this.gameObject);
+        subscribeForSceneChanging();
 	}
 	
-	
+    /// <summary>
+    /// Toutes les inscriptions aux changements de scene
+    /// </summary>
+	private void subscribeForSceneChanging()
+    {
+        EventManager.addActionToEvent<ScenesType>(EventType.END_SCENE, changeScene);
+    }
+
+    void changeScene(ScenesType newScene)
+    {
+
+    }
+
+
     void goToMainMenu()
     {
 
