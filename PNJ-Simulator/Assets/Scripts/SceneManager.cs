50df8631cdd215f6ebe1067373312c612a4cff5a
pingu111 <theo.carron@telecom-sudparis.eu>
2016-11-04 23:41:38 +0100

    Ok scenemanager

--- PNJ-Simulator/Assets/Scripts/SceneManager.cs
+++ PNJ-Simulator/Assets/Scripts/SceneManager.cs
@@ -1,6 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/// <summary>
+/// Different types of scenes
+/// </summary>
 public enum ScenesType
 {
     SHOP,
@@ -9,48 +12,66 @@ public enum ScenesType
     BATTLE
 }
 
+/// <summary>
+/// Manager of the scenes
+/// </summary>
 public class SceneManager : MonoBehaviour
 {
 	// Use this for initialization
 	void Start ()
     {
         DontDestroyOnLoad(this.gameObject);
-        subscribeForSceneChanging();
-	}
-	
-    /// <summary>
-    /// Toutes les inscriptions aux changements de scene
-    /// </summary>
-	private void subscribeForSceneChanging()
-    {
         EventManager.addActionToEvent<ScenesType>(EventType.END_SCENE, changeScene);
     }
 
+    /// <summary>
+    /// Switch between the scenes
+    /// </summary>
+    /// <param name="newScene"></param>
     void changeScene(ScenesType newScene)
     {
-
+        switch(newScene)
+        {
+            case ScenesType.MAIN_MENU:
+                goToMainMenu();
+                break;
+            case ScenesType.BATTLE:
+                goToBattle();
+                break;
+            case ScenesType.MAP:
+                goToMap();
+                break;
+            case ScenesType.SHOP:
+                goToShop();
+                break;
+            default:
+                Debug.Log("Probleme dans changeScene");
+                break;
+        }
     }
-
 
     void goToMainMenu()
     {
-
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenuScene");
     }
 
     void goToBattle()
     {
-
+        UnityEngine.SceneManagement.SceneManager.LoadScene("BattleScene");
     }
 
     void goToMap()
     {
-
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Mapscene");
     }
 
     void goToShop()
     {
-
+        UnityEngine.SceneManagement.SceneManager.LoadScene("ShopScene");
     }
 
-
+    void OnDestroy()
+    {
+        EventManager.removeActionFromEvent<ScenesType>(EventType.END_SCENE, changeScene);
+    }
 }
