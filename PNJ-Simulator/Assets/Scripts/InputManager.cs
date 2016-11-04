ba5ac9ca11f88706aaaa02287128a27d698fd379
Chewie <kevin.goilard@gmail.com>
2016-11-05 00:41:08 +0100

    Menu Input done TODO : Test it

--- PNJ-Simulator/Assets/Scripts/InputManager.cs
+++ PNJ-Simulator/Assets/Scripts/InputManager.cs
@@ -4,7 +4,17 @@ using System.Collections;
 public class InputManager : MonoBehaviour {
 
     private ScenesType currentType;
-    [SerializeField] private GameObject player;
+    [SerializeField] private Player player;
+    [SerializeField] private Menu menu;
+
+    private float horizontal;
+    private float vertical;
+    private float attack1;
+    private float attack2;
+    private float splash;
+    private float submit;
+
+    private bool SelectedOptionChanged = false;
 
 
 	// Use this for initialization
@@ -12,16 +22,52 @@ public class InputManager : MonoBehaviour {
         currentType = ScenesType.MAIN_MENU;
         DontDestroyOnLoad(this.gameObject);
         EventManager.addActionToEvent<ScenesType>(EventType.END_SCENE, OnSceneChanged);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	    
+        EventManager.addActionToEvent<Menu>(EventType.MENU_ENTERED, OnMenuEntered);
+        EventManager.addActionToEvent(EventType.MENU_ENTERED, OnMenuExit);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        attack1 = Input.GetAxis("Attack1");
+        attack2 = Input.GetAxis("Attack2");
+        splash = Input.GetAxis("Splash");
+        submit = Input.GetAxis("Submit");
+        switch (currentType)
+        {
+            case ScenesType.MAP:
+            case ScenesType.SHOP:
+                if (menu != null)
+                    OnMenu();
+                else
+                    OnMap();
+                break;
+            case ScenesType.MAIN_MENU:
+                OnMenu();
+                break;
+            case ScenesType.BATTLE:
+                OnBattle();
+                break;
+            default:
+                break;
+        }
+
 	}
 
-    void OnSceneChanged(ScenesType type)
+    private void OnSceneChanged(ScenesType type)
     {
         currentType = type;
+    }
+
+    private void OnMenuEntered(Menu m)
+    {
+        menu = m;
+    }
+
+    private void OnMenuExit()
+    {
+        menu = null;
     }
 
     void OnDestroy()
@@ -30,6 +76,29 @@ public class InputManager : MonoBehaviour {
     }
 
     private void OnMenu()
+    {
+        if (vertical != 0)
+        {
+            if (!SelectedOptionChanged)
+            {
+                if (vertical > 0)
+                    menu.decrementPosition();
+                else
+                    menu.incrementPosition();
+            }
+        }
+        else
+            SelectedOptionChanged = false;
+        if (submit != 0)
+            menu.call();
+    }
+
+    private void OnMap()
+    {
+
+    }
+
+    private void OnBattle()
     {
 
     }
