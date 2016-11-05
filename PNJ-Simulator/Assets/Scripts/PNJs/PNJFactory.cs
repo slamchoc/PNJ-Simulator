ab12f5f6d0bb3f86164c2003b72bf1b22d628cbd
pingu111 <theo.carron@telecom-sudparis.eu>
2016-11-05 02:35:06 +0100

    add

---
+++ PNJ-Simulator/Assets/Scripts/PNJs/PNJFactory.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+public class PNJFactory : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject prefabPNJ;
+
+    [SerializeField]
+    private GameObject prefabMonster;
+
+    // Use this for initialization
+    void Start ()
+    {
+        createPNJs();
+    }
+
+    private void createPNJs()
+    {
+        ;
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    void createPNJ(Vector3 position, Delegate[] menu)
+    {
+        GameObject pnj =  Instantiate(prefabPNJ);
+        pnj.GetComponent<PNJ>().setMenu(menu);
+        pnj.transform.position = position;
+    }
+
+    void createMonster(Vector3 position, Delegate[] menu, int pv,  Vector3 patternA, Vector3 patternB)
+    {
+        GameObject monster = Instantiate(prefabMonster);
+        monster.GetComponent<Monster>().Monster(pv, patternA, patternB);
+        monster.GetComponent<Monster>().setMenu(menu);
+        monster.transform.position = position;
+    }
+}
