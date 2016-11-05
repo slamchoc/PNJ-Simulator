562b106f2210ad75c8c9f5f835f767ba607b651f
pingu111 <theo.carron@telecom-sudparis.eu>
2016-11-05 09:35:24 +0100

    Pouit.

--- PNJ-Simulator/Assets/Scripts/Sounds/SoundManager.cs
+++ PNJ-Simulator/Assets/Scripts/Sounds/SoundManager.cs
@@ -1,5 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Collections;
 
 public enum Sounds
 {
@@ -17,19 +20,53 @@ public class SoundManager : MonoBehaviour {
 
     public Dictionary<string, Sound> actualSounds;
 
-	// Use this for initialization
-	void Start ()
+    private string absolutePath = "Assets/Sound";
+
+    private List<string> validExtensions = new List<string> { ".ogg", ".wav" }; 
+
+
+    // Use this for initialization
+    void Start ()
     {
         actualSounds = new Dictionary<string, Sound>();
+        translateAudioToString = new Dictionary<string, AudioClip>();
+
         DontDestroyOnLoad(this.gameObject);
 
-        foreach(AudioClip clip in sources)
+
+        // Loading of the sounds 
+        sources = new List<AudioClip>();
+        var info = new DirectoryInfo(absolutePath);
+        FileInfo[] sourcesFiles = info.GetFiles().Where(f => IsValidFileType(f.Name)).ToArray();
+        foreach (FileInfo s in sourcesFiles)
+            StartCoroutine(LoadFile(s.FullName));
+
+        foreach (AudioClip clip in sources)
         {
             translateAudioToString.Add(clip.name, clip); 
         }
 
         EventManager.addActionToEvent<Sounds>(EventType.PLAY_SOUND, playSound);
 	}
+
+    bool IsValidFileType(string fileName)
+    {
+        return validExtensions.Contains(Path.GetExtension(fileName));
+    }
+
+    IEnumerator LoadFile(string path)
+    {
+        WWW www = new WWW("file://" + path);
+        print("loading " + path);
+
+        AudioClip clip = www.GetAudioClip(false);
+        while (clip.loadState != AudioDataLoadState.Loaded)
+            yield return www;
+
+        print("done loading");
+        clip.name = Path.GetFileName(path);
+        sources.Add(clip);
+    }
 
     public void playSound(Sounds soundToPlay)
     {
