c9d911041af280de3921688c02b08187a5ae3397
Paul <paul.turpin@telecom-sudgrenoble.eu>
2016-11-05 03:49:21 +0100

    Menu qui marche

--- PNJ-Simulator/Assets/Scripts/MenuManager.cs
+++ PNJ-Simulator/Assets/Scripts/MenuManager.cs
@@ -1,9 +1,30 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
+using System;
 
 public class MenuManager : MonoBehaviour
 {
     private ScenesType actualScene;
+
+    [SerializeField]
+    private GameObject arrowFight;
+    [SerializeField]
+    private GameObject arrowDialogue;
+    [SerializeField]
+    private GameObject panelFight;
+    [SerializeField]
+    private GameObject panelDialogue;
+
+    private Menu mainMenu = new Menu
+                                    (
+                                        new List<Pair<Callback, String>> {
+                                                                            new Pair<Callback,String>(()=> { Debug.Log("game"); },"Game"),
+                                                                            new Pair<Callback,String>(()=> { Debug.Log("exit"); }, "Exit")
+                                                                        },
+                                        "mainMenu"
+                                    );
+
+    private List<Pair<Menu,GameObject>> currentMenuActive = new List<Pair<Menu,GameObject>>();
 
 	// Use this for initialization
 	void Start ()
@@ -11,12 +32,16 @@ public class MenuManager : MonoBehaviour
         DontDestroyOnLoad(this.gameObject);
         EventManager.addActionToEvent<ScenesType>(EventType.NEW_SCENE, sceneChanged);
         EventManager.addActionToEvent<Menu>(EventType.MENU_ENTERED, menuToPrint);
+        menuToPrint(mainMenu);
     }
 
     // Update is called once per frame
     void Update ()
     {
-	    
+	    foreach(Pair<Menu,GameObject> menu in currentMenuActive)
+        {
+            menu.Second.transform.position = new Vector3(menu.Second.transform.position.x , menu.Second.transform.position.y + menu.First.position, menu.Second.transform.position.z);
+        }
 	}
 
     void sceneChanged(ScenesType newScene)
@@ -48,16 +73,38 @@ public class MenuManager : MonoBehaviour
 
     void printMainMenu(Menu _menu)
     {
+        foreach (Pair<Callback, string> pair in _menu.options)
+        {
+            Instantiate(panelFight);
+            panelFight.GetComponentInChildren<TextMesh>().text = pair.Second;
+        }
+        GameObject arrow = Instantiate(arrowFight);
 
+        currentMenuActive.Add(new Pair<Menu, GameObject>(_menu, arrow));
     }
 
     void printDialogue(Menu _menu)
     {
+        foreach (Pair<Callback, string> pair in _menu.options)
+        {
+            Instantiate(panelFight);
+            panelFight.GetComponentInChildren<TextMesh>().text = pair.Second;
+        }
+        GameObject arrow = Instantiate(arrowFight);
 
+        currentMenuActive.Add(new Pair<Menu, GameObject>(_menu, arrow));
     }
 
     void printBattle(Menu _menu)
     {
+        foreach(Pair<Callback,string> pair in _menu.options)
+        {
+            Instantiate(panelFight);
+            panelFight.GetComponentInChildren<TextMesh>().text = pair.Second;
+        }
+        GameObject arrow = Instantiate(arrowFight);
+
+        currentMenuActive.Add(new Pair<Menu, GameObject>(_menu, arrow));
 
     }
 
