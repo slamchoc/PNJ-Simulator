a5e67e12623091e179102944c9019a8920cda688
pingu111 <theo.carron@telecom-sudparis.eu>
2016-11-05 12:13:19 +0100

    Cimenatique deplacee

---
+++ PNJ-Simulator/Assets/Scripts/ShopScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShopScript : MonoBehaviour {
+
+    public GameObject cinematique;
+
+    private bool needCinematique = true;
+
+    // Use this for initialization
+    void Start ()
+    {
+        EventManager.raise<SoundsType>(EventType.PLAY_SOUND_ONCE, SoundsType.MUSIQUE_CINEMATIQUE2);
+        cinematique.SetActive(false);
+        cinematique.transform.position = new Vector3(
+            -cinematique.GetComponent<SpriteRenderer>().bounds.size.x / 2 - Camera.main.aspect * Camera.main.orthographicSize,
+            0,
+            0
+            );
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if(needCinematique)
+        {
+            cinematique.SetActive(true);
+
+            // cinematique
+            cinematique.transform.position += new Vector3(
+                       Time.deltaTime * 2.0f,
+                       0,
+                       0
+                       );
+            if (cinematique.transform.position.x > cinematique.GetComponent<SpriteRenderer>().bounds.size.x / 2 + Camera.main.aspect * Camera.main.orthographicSize)
+            {
+                needCinematique = false;
+                cinematique.SetActive(false);
+                EventManager.raise(EventType.STOP_SOUND);
+            }
+        }
+    }
+}
