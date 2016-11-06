8e48f7c79486f929ca8aada527f82fc7753de91f
Chewie <kevin.goilard@gmail.com>
2016-11-06 13:09:36 +0100

    Added House interaction && moving animation to the hero

---
+++ PNJ-Simulator/Assets/Scripts/House.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class House : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+        Menu houseMenu = new Menu(
+                                        new List<Pair<Callback, String>> { new Pair<Callback, String>( () => { EventManager.raise(EventType.MENU_EXIT); }, "Continue...") }, "(* Mais on ne rentre pas chez les gens... *)"
+                                    );
+        this.gameObject.GetComponent<PNJ>().setMenu(houseMenu);
+    }
+}
