d8991eaf1db14e8d2041ed254be1f9851cb8d6e4
pingu111 <theo.carron@telecom-sudparis.eu>
2016-11-06 09:41:08 +0100

    debut batailles scriptees

---
+++ PNJ-Simulator/Assets/ScriptedBattle.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public enum ScriptedBattleType
+{
+    TPT,
+    HERODIE
+}
+
+public class ScriptedBattle : MonoBehaviour
+{
+    public Vector3 positionplayer;
+
+    public GameObject hero;
+
+    public ScriptedBattleType typeBattle;
+
+    void Start()
+    {
+        hero = FindObjectOfType<Hero>().gameObject;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+	
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<Player>() != null)
+        {
+            EventManager.raise(EventType.CINEMATIC_BEGIN);
+            collision.gameObject.GetComponent<Player>().goToGoal(positionplayer);
+
+            if(typeBattle == ScriptedBattleType.HERODIE)
+            {
+                hero.transform.position = new Vector3(10, 0, 0);
+                hero.GetComponent<Animator>().Play("SecondBattle");
+            }
+            else
+            {
+                hero.transform.position = new Vector3(5, 0, 0);
+                hero.GetComponent<Animator>().Play("FirstBattlePart1");
+            }
+        }
+    }
+
+
+}
