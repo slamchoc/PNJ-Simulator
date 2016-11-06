d047c03c75009d04ccb564a4cffe99036710df3f
Paul <paul.turpin@telecom-sudgrenoble.eu>
2016-11-06 14:30:02 +0100

    FIN

--- PNJ-Simulator/Assets/Scripts/Fin.cs
+++ PNJ-Simulator/Assets/Scripts/Fin.cs
@@ -3,13 +3,30 @@ using System.Collections;
 
 public class Fin : MonoBehaviour {
 
+    bool defiler = false;
+    float speed = 0.5f;
+
 	// Use this for initialization
 	void Start () {
-	
+        EventManager.addActionToEvent(EventType.WIN,print);
 	}
 	
+    void print()
+    {
+        gameObject.GetComponent<SpriteRenderer>().enabled = true;
+        defiler = true;
+    }
+
 	// Update is called once per frame
 	void Update () {
-	
+        if (defiler)
+        {
+            transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y - (speed * Time.deltaTime), transform.localPosition.z);
+            if(transform.localPosition.y >= 6)
+            {
+                Debug.Log("FIN !");
+                Application.Quit();
+            }
+        }
 	}
 }
